be045aa32d40fbd3523edc0afa7784aca63e3b6b
Boyan Vassilev <vassilevb@gmail.com>
2023-06-16 10:22:42 +0300

    start

---
+++ generators/generate-digit-2.js
@@ -0,0 +1,21 @@
+function generateSecondDigit(generatedFirstDigit) {
+    if (generatedFirstDigit == 0) {
+        return 9;
+    }
+    const customerSex = prompt("Какъв е твоят пол M/F");
+    if (customerSex == "M") return 1;
+    if (customerSex == "F") return 2;
+    
+    // var generatedNumber = 0;
+    // do {
+
+    //     generatedNumber = getRandom(1, 9);
+
+    // } while (generatedNumber == 1 || generatedNumber == 3);
+
+    var generatedNumber = getRandom (1, 9);
+    // if (generatedNumber == 1) return generatedNumber++;
+    // if (generatedNumber == 3) return generatedNumber++;
+    if (generatedNumber ==1 || generatedNumber == 3) return generatedNumber++;
+    generatedNumber;
+}
